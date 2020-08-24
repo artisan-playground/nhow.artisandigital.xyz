a1361a331dd2b583cf82877556b51ca67dd22b6a
supreme5678 <supreme5678@users.noreply.github.com>
2020-08-24 08:51:38 +0000

    deploy: 6576ff933f93ad20847340359f73cbd20e644b71

---
+++ precache-manifest.c2e2dd9eab4ca718bcd328a70e681ece.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e1f21ec9f66641e413",
+    "url": "/css/app.325f2b15.css"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "993114fe7e183d26cae69a3502df15e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e1f21ec9f66641e413",
+    "url": "/js/app.bd16dbb6.js"
+  },
+  {
+    "revision": "66f3fd27ff6e9e9c8388",
+    "url": "/js/chunk-vendors.d6cb6766.js"
+  },
+  {
+    "revision": "f1fc28882d9f26aa1117",
+    "url": "/js/common.2181896e.js"
+  },
+  {
+    "revision": "69ddadb59da6451f24d0",
+    "url": "/js/dashboard.aed967dc.js"
+  },
+  {
+    "revision": "f8e80f5efd288bbba0d990fd82c74372",
+    "url": "/manifest.json"
+  }
+]);
